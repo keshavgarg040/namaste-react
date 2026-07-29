8345d50ef86b32e7f5c0dae93d0558a635c34ab6
keshav garg <keshavgarg040@gmail.com>
2026-07-29 07:23:07 +0530

    NamasteReactEp3part4

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-{/* <div id="parent">
+{
+  /* <div id="parent">
 <div id "child">
 <h1>I'm h1 tag</h1>
 </div>
-</div> */}
+</div> */
+}
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1",{id:"heaading",xyz:"abc"},"Hello World in React");
-const root = ReactDOM.createRoot(document.getElementById("container"));
-root.render(heading);
-
-
-const div=React.createElement("div",{id:"parent"},
-    [React.createElement("div",{id:"child1"},
-        [React.createElement("h1",{},"I'm h112255 tag"),
-            React.createElement("h2",{},"I am h2 tag")])
-        ,React.createElement("div",{id:"child2"},
-        [React.createElement("h1",{},"I'm h1 tag"),
-            React.createElement("h2",{},"I am h2 tag")]
-        )]);
-root.render(div);
-
+const heading = React.createElement("h1", { id: "heaading" }, "NaMAsteReact");
 console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//jsx => transpiled by babel => React.createElement => React Element (js object) =>Html element(render)
+const jsxheading = <h1 className="heaading">Namaste react using jsx gyugyg</h1>;
+console.log(jsxheading);
+root.render(jsxheading);
