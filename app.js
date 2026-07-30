e9c70c48dd1fb3c967ce656c08524fd6ecbd77e9
keshav garg <keshavgarg040@gmail.com>
2026-07-30 09:04:25 +0530

    nmaste reacr episode 3 part 3

--- app.js
+++ app.js
@@ -7,11 +7,15 @@
 }
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const heading = React.createElement("h1", { id: "heaading" }, "NaMAsteReact");
-console.log(heading);
+const Heading=()=>{return<h2>hello all keshav this side </h2>;};
+const HeadingComponent=() => (<div id = "container"><Heading/>  
+<h1 id ="heaading">
+  
+  NAmaste React12 Functionl Component
+  </h1></div>);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //jsx => transpiled by babel => React.createElement => React Element (js object) =>Html element(render)
 const jsxheading = <h1 className="heaading">Namaste react using jsx gyugyg</h1>;
 console.log(jsxheading);
-root.render(jsxheading);
+root.render(<HeadingComponent/>);
+   
