6c71276c959365fd3e9cdd602bcb6afaadf1f3bd
keshav garg <keshavgarg040@gmail.com>
2026-08-04 05:57:53 +0530

    2ndmethod-to-addimage-in-website-after-downloading-in-vscode

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const logo = new URL( "./public/logo.jpg",import.meta.url);
+import logo from "url:./public/logo.jpg";
 
 const Header = ()=>{
   return (<div className="header">
-    <img className="head-logo"src={logo.href} alt="logo" />
+    <img className="head-logo"src={logo} alt="logo" />
   <div className="header-list">
     <ul>
       <li>Home</li>
